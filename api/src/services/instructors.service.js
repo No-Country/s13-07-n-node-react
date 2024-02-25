d38cb999c7fe8c1a5e9e6dd46f2b7d5921208421
Emanuel Gauler <emanuelgauler@gmail.com>
2024-02-25 11:58:04 -0300

    Update instructors.service.js
    Se eliminó el mensaje para depuración

--- api/src/services/instructors.service.js
+++ api/src/services/instructors.service.js
@@ -12,7 +12,6 @@ function add_rating_to(instructor) {
 export class InstructorService {
    async instructor_role_id() {
       const roles = await rolServiceFilter( 'profesor/a' )
-      console.log( roles )
       return roles[0]._id
    }
 
