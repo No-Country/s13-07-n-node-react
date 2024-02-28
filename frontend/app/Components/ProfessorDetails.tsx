24e365b6a31261d39032f990c0fc5a9a94f039a0
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2024-02-28 10:47:13 -0400

    working yet

--- frontend/app/Components/ProfessorDetails.tsx
+++ frontend/app/Components/ProfessorDetails.tsx
@@ -3,11 +3,13 @@ import Image from 'next/image';
 import React, { useState } from 'react'
 import X from "../../public/x.png"
 import start from "../../public/star.png"
-import starColor from "../../public/star-color.png"
 import { useRouter } from 'next/navigation';
 import Loader from './Loader';
 import { urlAPi } from '../utils/urlBase';
 import { useFetchDataGet } from '../utils/useFetchDataGet';
+import ReviewsProfessor from './ReviewsProfessor';
+import StarRating from './StarRating';
+import CommentProfessor from './CommentProfessor';
 
 
 function ProfessorDetails({ params }: { params: {profesor: string;}}) {
@@ -16,14 +18,20 @@ function ProfessorDetails({ params }: { params: {profesor: string;}}) {
   const router = useRouter();
   const urlToFetch = `${urlAPi}/instructors/${params.profesor}`;
   const {data, isLoading, error} = useFetchDataGet(urlToFetch);
+  const [showReviews, setShowReviews] = useState(false);
+  const [showComment, setShowComment] = useState(false);
+  const [ratingProfessor, setRatingProfessor] = useState<number>(0);
+
+
   if (isLoading) return <div className='flex items-center justify-center h-screen'><Loader/></div> ;
   if (error) return <p className='flex items-center justify-center h-screen'>Error: {error}</p>;
   if (!data) return <p className='flex items-center justify-center h-screen'>No profile data</p>;
 
-  console.log(data.data);
+  console.log(ratingProfessor);
   return (
-    <div className={`w-full h-full  fixed  left-0 right-0 bottom-0  backdrop-blur-[5px]  bg-opacity-60`}>
-      
+    <div className={`w-full h-full  fixed  left-0 right-0 bottom-0 top-0`}>
+      {showComment === true &&(<CommentProfessor closeModal={()=>{setShowComment(false)}} />)}
+      {showReviews === true &&(<ReviewsProfessor closeModal={()=>{setShowReviews(false)}} />)}
       <button onClick={()=> {router.push('/Profesores')}} >
         <Image src={X} alt="x" className="w-[24px] h-[24px] absolute right-5 top-5" />
       </button>
@@ -42,7 +50,7 @@ function ProfessorDetails({ params }: { params: {profesor: string;}}) {
                 
               </div>
               <div>
-                <p className='text-primaryDefault font-semibold text-right'>Ver reseñas</p>
+                <button onClick={()=>{setShowReviews(!showReviews)}} className='cursor-pointer text-primaryDefault font-semibold text-right w-full'>Ver reseñas</button>
                 <p className="mt-[30px] font-semibold  whitespace-nowrap overflow-hidden text-overflow-ellipsis w-full">
                   {data.data.description}
                 </p>
@@ -65,15 +73,11 @@ function ProfessorDetails({ params }: { params: {profesor: string;}}) {
               <div>
                 <p className="mt-[30px]">
                   <p className="flex justify-center items-center">
-                    <Image className="w-[24px] h-[24px] mx-2" src={starColor} alt="star-color" />
-                    <Image className="w-[24px] h-[24px] mx-2" src={starColor} alt="star-color" />
-                    <Image className="w-[24px] h-[24px] mx-2" src={starColor} alt="star-color" />
-                    <Image className="w-[24px] h-[24px] mx-2" src={starColor} alt="star-color" />
-                    <Image className="w-[24px] h-[24px] mx-2" src={starColor} alt="star-color" />
+                    <StarRating ratingProfessor={ratingProfessor} onRatingChange={setRatingProfessor}/>
                   </p>
-                  <p className="mt-[16px] font-semibold">Deja un comentario</p>
+                  <p onClick={()=>{setShowComment(!showComment)}} className="mt-[36px] font-semibold">Deja un comentario</p>
                 </p>
-                <button onClick={() => { setFlujo(calificacion[2]); } } type="button" className="mt-[12px] text-white w-full focus:outline-none  bg-primaryDefault hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">
+                <button onClick={() => { setFlujo(calificacion[2]); } } type="button" className="mt-[36px] text-white w-full focus:outline-none  bg-primaryDefault hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">
                   Enviar
                 </button>
               </div>
