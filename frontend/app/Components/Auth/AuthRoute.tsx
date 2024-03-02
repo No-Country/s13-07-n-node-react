3669afb1fe51ecced5d90594c05daa24b31fa447
Emi Cabanillas <emicabanillas.cab@gmail.com>
2024-03-02 20:49:43 -0300

    fix authrouter with usePathname

--- frontend/app/Components/Auth/AuthRoute.tsx
+++ frontend/app/Components/Auth/AuthRoute.tsx
@@ -1,10 +1,14 @@
 "use client"
 import { useGlobalStore } from "@/app/store/GlobalStore";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname  } from "next/navigation";
 import { useEffect } from "react";
 
 const AuthRoute = ({ children }: {children : any}) => {
+    const router = useRouter();
+    const pathname = usePathname();
     const isAuthClient = useGlobalStore((state) => state.isAuthClient);
+   
+    useEffect(() => !isAuthClient ? router.push('/acceso') : router.push(pathname), [pathname]);
 
     return isAuthClient ? children : null;
 };
