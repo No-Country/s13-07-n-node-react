dec92d0c7dbe8e4ca46f28d5c89d722e5e914f42
Emi Cabanillas <emicabanillas.cab@gmail.com>
2024-03-02 20:01:50 -0300

    fix AuthRoute

--- frontend/app/Components/Auth/AuthRoute.tsx
+++ frontend/app/Components/Auth/AuthRoute.tsx
@@ -6,16 +6,6 @@ import { useEffect } from "react";
 const AuthRoute = ({ children }: {children : any}) => {
     const isAuthClient = useGlobalStore((state) => state.isAuthClient);
 
-<<<<<<< Updated upstream
-    useEffect(() => {
-        if (!isAuthClient) {
-            router.push('/acceso');
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isAuthClient]);
-
-=======
->>>>>>> Stashed changes
     return isAuthClient ? children : null;
 };
 
