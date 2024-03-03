5bb1411cf819235a0a2488e5c5bce84947e3d3f6
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2024-03-02 21:07:31 -0400

    working yet

--- frontend/app/Components/Auth/AuthRoute.tsx
+++ frontend/app/Components/Auth/AuthRoute.tsx
@@ -7,9 +7,8 @@ const AuthRoute = ({ children }: {children : any}) => {
     const router = useRouter();
     const pathname = usePathname();
     const isAuthClient = useGlobalStore((state) => state.isAuthClient);
-   
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => !isAuthClient ? router.push('/acceso') : router.push(pathname), [pathname]);
+    useEffect(() => !isAuthClient ? router.push('/acceso') : router.push(pathname), [pathname, isAuthClient]);
 
     return isAuthClient ? children : null;
 };
