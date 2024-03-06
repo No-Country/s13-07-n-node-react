a4d6d74adeed44fb6cdab36beea223cbfabbe8e1
emanuelgauler <emanuelgauler@gmail.com>
2024-03-05 21:32:54 -0300

    se agregó validación para la publicación de reseñas

--- api/src/routes/instructors.routes.js
+++ api/src/routes/instructors.routes.js
@@ -1,4 +1,37 @@
 import { InstructorsController } from "../controllers/instructors.controller.js";
+import { checkSchema } from 'express-validator'
+
+const review_post_validation = function() {
+  return checkSchema( {
+    reviewer: {
+      trim: true
+      , notEmpty: { errorMessage: 'Debe ingresar el nombre del revisor' }
+      //, isAlpha: { errorMessage: 'Debe contener únicamente caracteres visible no símbolos'}
+    }
+    , rating: {
+      trim: true
+      , notEmpty: { errorMessage: 'Debe ingresar un puntaje'}
+      , isInt: { 
+        options: { min: 1, max: 5}
+        , errorMessage: 'El puntaje debe estar entre 1 y 5'
+      }
+    }
+    , client: {
+      trim: true
+      , notEmpty: { errorMessage: 'Debe ingresar el ID del cliente' }
+      , isMongoId: { errorMessage: 'Debe ser un ID válido para MongoDB' }
+    }
+    , comment: {
+      optional: true
+      , trim: true
+      , isLength: {
+        options: { max: 150 }
+        , errorMessage: 'No debe exceder los 150 caracteres'
+      }
+      , isAlpha: { errorMessage: 'Solo debe contener caracteres legibles' }
+    }
+  }, ['body'])
+}
 
 const instructors = new InstructorsController();
 
@@ -13,6 +46,6 @@ export class Instructors {
     router.patch(AN_INSTRUCTOR_PATH, instructors.update);
     router.delete(AN_INSTRUCTOR_PATH, instructors.destroy);
     router.get(REVIEW_PATH, instructors.show_reviews);
-    router.post(REVIEW_PATH, instructors.enter_review);
+    router.post(REVIEW_PATH, review_post_validation(), instructors.enter_review);
   }
 }
