b01b78a87994a4c3e8f2d599e692610d14a52dd0
Isaac Urdaneta <urdanetacuarteisaacdavid@gmail.com>
2024-03-07 08:23:26 -0400

    refactorizando

--- frontend/app/Components/CardContainerProfessor.tsx
+++ frontend/app/Components/CardContainerProfessor.tsx
@@ -4,6 +4,7 @@ import { urlAPi } from '../utils/urlBase';
 import { useFetchDataGet } from '../utils/useFetchDataGet';
 import Loader from './Loader';
 
+
 function CardContainerProfessor() {
     //ahora vamos a pedir el total de profesores
     const urlToFetch = `${urlAPi}/instructors`;
@@ -12,10 +13,11 @@ function CardContainerProfessor() {
     if (isLoading) return <div className='flex items-center justify-center p-2'><Loader/></div> ;
     if (error) return <p className='flex items-center justify-center h-screen'>Error: {error}</p>;
     if (!data) return <p className='flex items-center justify-center h-screen'>No profile data</p>;
+    console.log(data)
   return (
     <div className='w-[100%] grid grid-cols-12 mt-[24px]'>
-      {
-        data.data.map((profesor: any, key: React.Key | null | undefined)=>{
+      
+        {/*data.data.map((profesor: any, key: React.Key | null | undefined)=>{
           //console.log(profesor)
           const {firstName,lastName,  active, rating, image, id} =  profesor;
           return(
@@ -23,8 +25,9 @@ function CardContainerProfessor() {
               <CardProfessor id={id}  rate={rating} image={image} lastName={lastName} firstName={firstName} available={active}/>
             </div>
           )
-        })
-      }
+        })*/}
+        
+      
     </div>
     
     
